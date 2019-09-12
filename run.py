aceacd9100153e8de197e2179265870fcc800247
Mani <mani.ahmadi@ssense.com>
2019-09-11 23:19:56 -0400

    style

--- run.py
+++ run.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return 'home page'
+
 
 @app.route('/hello-view/<name>')
 def load_view(name):
@@ -16,12 +18,14 @@ def load_view(name):
 
     return render_template('hello.html', **data)
 
+
 @app.route('/json')
 def json_data():
     return {
         'health': 'passed',
         'rest': 'unknown'
     }
+
 
 if __name__ == '__main__':
     app.run()
